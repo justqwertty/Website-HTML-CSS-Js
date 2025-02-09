4628d40d259152b8ecfb7a1ed57e1ac0b1201c09
Omar Hisham <85786986+justqwertty@users.noreply.github.com>
2025-02-09 05:44:08 +0200

    Fixed firstbug where the boxes would collide when on smaller screens :)

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
+
+    // Hide all tab content
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
+
+    // Remove the "active" class from all buttons
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    // Show the current tab content and mark the button as active
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
-  
-// Get the element with id="defaultOpen" and click on it
+}
+
+// Open the default tab on page load
 document.getElementById("defaultOpen").click();
